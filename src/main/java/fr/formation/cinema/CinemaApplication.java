fda26467aa1bcdcd0661c2ea24fa7abd575fb38e
Bill29200 <nabil.kadjouh@gmail.com>
2022-01-19 16:32:53 +0100

    V2 Film complet avec des ecrans CRUD

--- src/main/java/fr/formation/cinema/CinemaApplication.java
+++ src/main/java/fr/formation/cinema/CinemaApplication.java
@@ -50,50 +50,54 @@ public class CinemaApplication implements CommandLineRunner{
 	@Transactional
 	public void run(String... args) throws Exception {
 		
-		/*
-		 * System.out.println(
-		 * "£££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££"
-		 * );
-		 * 
-		 * Film f1 =new Film("Training day", "Policier", "Denzel washigton",180,
-		 * "C'est film d'un nouveau policier avec un ancien policier traitre complisse avec une bande de drogue "
-		 * ); Film f2 =new Film("Brave Heart", "Historique", "Mel Gibson",200,
-		 * "Un film qui raconte l'histoire de l'icosse avec la grande bretagne et represente l'hero William Wallas "
-		 * ); Film f3 =new Film("Titanic", "Romantique", "Cameroon",180,
-		 * "l'histoire de la fin d'un grand bateau qui vas traverser l'atlantique de l'angleterre vers l'amerique "
-		 * ); filmManager.add(f1);filmManager.add(f2);filmManager.add(f3);
-		 * 
-		 * //............................................
-		 * 
-		 * Salle s1= new Salle("Grande Salle", "Adult", 400); salleManager.add(s1);
-		 * Salle s2= new Salle("La centrale", "Jeune", 500); salleManager.add(s2); Salle
-		 * s3= new Salle("SalleToons", "Enfant", 100); salleManager.add(s3);
-		 * 
-		 * //............................................
-		 * 
-		 * Seance se1 = new Seance(null,null,null,12,f1, s1);seanceManager.add(se1);
-		 * Seance se2 = new Seance(null,null,null,20,f2, s2);seanceManager.add(se2);
-		 * Seance se3 = new Seance(null,null,null,20,f1,s3);seanceManager.add(se3);
-		 * Seance se4 = new Seance(null,null,null,20,f2, s3);seanceManager.add(se4);
-		 * 
-		 * //............................................
-		 * 
-		 * Client c1 = new Client("Mohamad","Ali","0784512698"); clientManager.add(c1);
-		 * Client c2 = new Client("Mike","Tysson","0654978102"); clientManager.add(c2);
-		 * Client c3 = new Client("Fatima","Sabri","0521369874"); clientManager.add(c3);
-		 * //............................................
-		 * 
-		 * Reserver r1 = new Reserver(c1,se1); reserverManager.add(r1); Reserver r2 =
-		 * new Reserver(c1,se2); reserverManager.add(r2); Reserver r3 = new
-		 * Reserver(c2,se1); reserverManager.add(r3); Reserver r4 = new
-		 * Reserver(c2,se2); reserverManager.add(r4); Reserver r5 = new
-		 * Reserver(c3,se3); reserverManager.add(r5); Reserver r6 = new
-		 * Reserver(c3,se4); reserverManager.add(r6);
-		 * 
-		 * System.out.println(
-		 * "£££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££"
-		 * );
-		 */
+		
+		  System.out.println("£££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££");
+		 
+		 Film f1 =new Film("Training day", "Policier", "Denzel washigton",180, "C'est film d'un nouveau policier avec un ancien policier traitre complisse avec une bande de drogue " );filmManager.add(f1);
+		 Film f2 =new Film("Brave Heart", "Historique", "Mel Gibson",200,
+		  "Un film qui raconte l'histoire de l'icosse avec la grande bretagne et represente l'hero William Wallas "
+		  ); Film f3 =new Film("Titanic", "Romantique", "Cameroon",180,
+		  "l'histoire de la fin d'un grand bateau qui vas traverser l'atlantique de l'angleterre vers l'amerique "
+		  ); filmManager.add(f1);filmManager.add(f2);filmManager.add(f3);
+		  
+		  //............................................
+		  
+			/*
+			 * Salle s1= new Salle("Grande Salle", "Adult", 400); salleManager.add(s1);
+			 * Salle s2= new Salle("La centrale", "Jeune", 500); salleManager.add(s2); Salle
+			 * s3= new Salle("SalleToons", "Enfant", 100); salleManager.add(s3);
+			 */
+		  
+		  //............................................
+		  
+			/*
+			 * Seance se1 = new Seance(null,null,null,12,f1, s1);seanceManager.add(se1);
+			 * Seance se2 = new Seance(null,null,null,20,f2, s2);seanceManager.add(se2);
+			 * Seance se3 = new Seance(null,null,null,20,f1,s3);seanceManager.add(se3);
+			 * Seance se4 = new Seance(null,null,null,20,f2, s3);seanceManager.add(se4);
+			 */
+		  
+		  //............................................
+		  
+			/*
+			 * Client c1 = new Client("Mohamad","Ali","0784512698"); clientManager.add(c1);
+			 * Client c2 = new Client("Mike","Tysson","0654978102"); clientManager.add(c2);
+			 * Client c3 = new Client("Fatima","Sabri","0521369874"); clientManager.add(c3);
+			 */
+		  //............................................
+		  
+			/*
+			 * Reserver r1 = new Reserver(c1,se1); reserverManager.add(r1); Reserver r2 =
+			 * new Reserver(c1,se2); reserverManager.add(r2); Reserver r3 = new
+			 * Reserver(c2,se1); reserverManager.add(r3); Reserver r4 = new
+			 * Reserver(c2,se2); reserverManager.add(r4); Reserver r5 = new
+			 * Reserver(c3,se3); reserverManager.add(r5); Reserver r6 = new
+			 * Reserver(c3,se4); reserverManager.add(r6);
+			 */
+		  
+		  System.out.println("£££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££££"
+		  );
+		 
 	}
 
 }
--- src/main/java/fr/formation/cinema/ihm/FilmController.java
+++ src/main/java/fr/formation/cinema/ihm/FilmController.java
@@ -1,8 +1,11 @@
 package fr.formation.cinema.ihm;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +20,57 @@ public class FilmController {
 	@Autowired
 	FilmManager filmManager;
 	
-	@GetMapping("/index")
+	@GetMapping("/film")
 	public String index(Model model) {
-		model.addAttribute("lstFilm",filmManager.getAll());
-		return "index";
+		model.addAttribute("lstFilms",filmManager.getAll());
+		return "film";
 	}
 	
+	@GetMapping("/detailsFilm/{id}")
+	public String detailsFilm(Model model,@PathVariable("id") Integer id) {
+		Film film = filmManager.getById(id);
+				
+		model.addAttribute("film",film);
+		model.addAttribute("mode","detailsFilm");
+		return "detailsFilm";
+	}
+	
+	@GetMapping("/addFilm")
+	public String addFilm(Model model,Film film) {
+		model.addAttribute("id",0); // j'envoie un attribut id à 0 pour que l'url de retour du formulaire soit conforme à celle du modify
+		model.addAttribute("mode","addFilm");
+		return "filmForm";
+	}
+	
+	@GetMapping("/deleteFilm/{id}")
+	public String deleteFilm(@PathVariable("id") Integer id) {
+		filmManager.deleteById(id);
+		return "redirect:/film";
+	}
+	@PostMapping("/valid/{mode}/{id}")
+	public String valid(Model model, @Valid Film film, BindingResult result, @PathVariable String mode, @PathVariable Integer id) {
+		if(result.hasErrors()) {
+			return "filmForm";
+		}
+		if("addFilm".equals(mode)) {
+			filmManager.add(film);
+			
+		}
+		else if("modifyFilm".equals(mode)) {
+			film.setIdFilm(id); 
+			filmManager.modify(film);
+			
+		}
+		return "redirect:/film";
+	}
+	
+	@GetMapping("/modifyFilm/{id}")
+	public String modifyFilm(Model model,@PathVariable("id") Integer id, Film film) {
+		film = filmManager.getById(id); 
+		model.addAttribute("mode","modifyFilm");
+		model.addAttribute("film",film); 
+		model.addAttribute("id",id);
+		return "filmForm";
+	}
 
 }
